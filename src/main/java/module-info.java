3215fecfe1228398bf81a290831bc66d5dbfec3b
JOSE RODRIGUEZ <jrodriguezg53@miumg.edu.gt>
2026-05-04 00:14:10 -0600

    Interfaces Registro e Inicio de sesion empezadas

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module umg.proyectomultiterminal {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires java.logging;
+    requires java.base;
 
     opens umg.proyectomultiterminal to javafx.fxml;
     exports umg.proyectomultiterminal;
